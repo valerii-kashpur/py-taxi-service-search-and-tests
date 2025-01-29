9a077100f8a0acd3fc0f8bd373c56da9c2a7e839
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-01-29 16:41:24 +0200

    search bars

--- taxi/forms.py
+++ taxi/forms.py
@@ -17,6 +17,10 @@ class CarForm(forms.ModelForm):
         fields = "__all__"
 
 
+class CarModelSearchForm(forms.Form):
+    model = forms.CharField(max_length=255, required=False)
+
+
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
@@ -30,6 +34,10 @@ class DriverCreationForm(UserCreationForm):
         return validate_license_number(self.cleaned_data["license_number"])
 
 
+class DriverUsernameSearchForm(forms.Form):
+    username = forms.CharField(max_length=255, required=False)
+
+
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = Driver
@@ -39,8 +47,12 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         return validate_license_number(self.cleaned_data["license_number"])
 
 
+class ManufacturerNameSearchForm(forms.Form):
+    name = forms.CharField(max_length=255, required=False)
+
+
 def validate_license_number(
-    license_number,
+        license_number,
 ):  # regex validation is also possible here
     if len(license_number) != 8:
         raise ValidationError("License number should consist of 8 characters")
